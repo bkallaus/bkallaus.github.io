b6c91972fea29472b712c9610738599a3d723d72
bkallaus <benkallaus@gmail.com>
2023-10-14 17:43:17 -0500

    feat: convert to functional

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,34 +1,34 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Footer extends Component {
-  render() {
-    if (this.props.sharedBasicInfo) {
-      var networks = this.props.sharedBasicInfo.social.map(function (network) {
-        return (
-          <span key={network.name} className="m-4">
-            <a href={network.url} target="_blank" rel="noopener noreferrer">
-              <i className={network.class}></i>
-            </a>
-          </span>
-        );
-      });
-    }
+const Footer = ({ sharedBasicInfo }) => {
+  let networks;
 
+if(sharedBasicInfo){
+   networks = sharedBasicInfo.social.map(function (network) {
     return (
-      <footer>
-        <div className="col-md-12">
-          <div className="social-links">{networks}</div>
-          <div className="copyright py-4 text-center">
-            <div className="container">
-              <small>
-                Template From <a href="https://github.com/Dorota1997/react-frontend-dev-portfolio" >Dorota1997</a>
-              </small>
-            </div>
-          </div>
-        </div>
-      </footer>
+      <span key={network.name} className="m-4">
+        <a href={network.url} target="_blank" rel="noopener noreferrer">
+          <i className={network.class}></i>
+        </a>
+      </span>
     );
-  }
+  });
 }
+
+return (
+  <footer>
+    <div className="col-md-12">
+      <div className="social-links">{networks}</div>
+      <div className="copyright py-4 text-center">
+        <div className="container">
+          <small>
+            Template From <a href="https://github.com/Dorota1997/react-frontend-dev-portfolio" >Dorota1997</a>
+          </small>
+        </div>
+      </div>
+    </div>
+  </footer>
+);
+};
 
 export default Footer;
